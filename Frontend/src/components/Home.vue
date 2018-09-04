c9f9b10933097de2ca111f304194b2b8cae7c1f9
Michael Massey <michaelmassey100@gmail.com>
2018-09-04 12:33:43 +0100

    fixed carousel

--- Frontend/src/components/Home.vue
+++ Frontend/src/components/Home.vue
@@ -33,13 +33,8 @@
 </template>
 
 <script>
-import { Carousel, Slide } from 'vue-carousel'
 export default {
-    name: 'Home',
-    components: {
-        Carousel,
-        Slide
-    }
+    name: 'Home'
 }
 </script>
 
